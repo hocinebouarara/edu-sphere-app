1556439e20904adc9bd91be912520894b053cac3
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-10 16:31:02 +0100

    intitialze the spring boot app

---
+++ src/main/java/com/hocinebouarara/coursescraze/CoursesCrazeApplication.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.coursescraze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesCrazeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesCrazeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hocinebouarara/coursescraze/CoursesCrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hocinebouarara.coursescraze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesCrazeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
