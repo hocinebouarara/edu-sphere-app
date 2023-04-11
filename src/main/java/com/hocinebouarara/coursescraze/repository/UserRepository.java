311c484e610b4c2d2500c07d013d997b4a187c8c
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-11 12:45:47 +0100

    add query find user by email

--- src/main/java/com/hocinebouarara/coursescraze/repository/UserRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.hocinebouarara.coursescraze.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // add custom query methods, if needed
+    // add custom query methods, if
+
+    Optional<User> findByEmail(String email);
+
 }
