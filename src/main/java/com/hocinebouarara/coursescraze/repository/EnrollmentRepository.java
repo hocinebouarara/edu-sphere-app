e467b7b08c24f00e0d51bc7c2e6211832c317b31
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-11 12:47:16 +0100

    add query get enrollment by course

--- src/main/java/com/hocinebouarara/coursescraze/repository/EnrollmentRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/EnrollmentRepository.java
@@ -4,6 +4,10 @@ import com.hocinebouarara.coursescraze.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
+
+    List<Enrollment> getEnrollmentByCourseId(Long courseId);
 }
