d3547c445d59efbd2dee7be3ae2b104f29873a66
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-11 12:46:28 +0100

    add query find review by course

--- src/main/java/com/hocinebouarara/coursescraze/repository/ReviewRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/ReviewRepository.java
@@ -4,6 +4,10 @@ import com.hocinebouarara.coursescraze.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
+
+    List<Review> getReviewByCourseId(Long courseId);
 }
