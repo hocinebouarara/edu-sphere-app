57fb24fb22178d4b26078a4c557a2fd69693d508
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-12 18:07:57 +0100

    rebuild the course repository

--- src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
@@ -1,18 +1,12 @@
 package com.hocinebouarara.coursescraze.repository;
 
-import com.hocinebouarara.coursescraze.entity.Category;
 import com.hocinebouarara.coursescraze.entity.Course;
-import com.hocinebouarara.coursescraze.entity.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course ,Long> {
-
-    List<Course> findByProvider(Provider provider);
-
-    List<Course> findByCategory(Category category);
 
 }
