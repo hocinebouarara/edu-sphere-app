15943beba1cb1f9dec9d19e57548fa3aa2f842d4
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-13 15:02:12 +0100

    integrating the angular and spring boot app with add the crossOrigin to the repositories

--- src/main/java/com/hocinebouarara/coursescraze/repository/CourseCategoryRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/CourseCategoryRepository.java
@@ -4,8 +4,10 @@ import com.hocinebouarara.coursescraze.entity.CourseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "courseCategory",path = "course-category")
 public interface CourseCategoryRepository extends JpaRepository<CourseCategory,Long> {
 }
--- src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
@@ -4,9 +4,11 @@ import com.hocinebouarara.coursescraze.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface CourseRepository extends JpaRepository<Course ,Long> {
 
 }
