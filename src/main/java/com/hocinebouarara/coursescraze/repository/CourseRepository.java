74915320c09ad36a3685e1e71d67d73b7e5c2ba6
hocinebouarara <hocinbouarara08@gmail.com>
2023-04-17 16:31:37 +0100

    add query for get courses by category id

--- src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
+++ src/main/java/com/hocinebouarara/coursescraze/repository/CourseRepository.java
@@ -1,14 +1,19 @@
 package com.hocinebouarara.coursescraze.repository;
 
 import com.hocinebouarara.coursescraze.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface CourseRepository extends JpaRepository<Course ,Long> {
+
+    Page<Course> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
