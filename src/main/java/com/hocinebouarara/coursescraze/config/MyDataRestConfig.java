aa7d3838cbfad8dd08a49d3f5a0337d2d659b852
hocinebouarara <hocinbouarara08@gmail.com>
2023-05-08 22:28:42 +0100

    disable http methods for course-category and course: (put, post and delete)

---
+++ src/main/java/com/hocinebouarara/coursescraze/config/MyDataRestConfig.java
@@ -0,0 +1,49 @@
+package com.hocinebouarara.coursescraze.config;
+
+import com.hocinebouarara.coursescraze.entity.Course;
+import com.hocinebouarara.coursescraze.entity.CourseCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod [] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+
+        // disable http methods for course: put, post, delete
+        config.getExposureConfiguration()
+                .forDomainType(Course.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable http methods for courseCategory: put, post, delete
+        config.getExposureConfiguration()
+                .forDomainType(CourseCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+    }
+
+
+
+
+}
